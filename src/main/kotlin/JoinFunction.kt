f14741fa80205cecf082b99dfeb1849a602f4ab0
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-09-12 19:41:47 +0530

    Coroutine Join Function

---
+++ src/main/kotlin/JoinFunction.kt
@@ -0,0 +1,22 @@
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking{
+    // Create a blocking coroutine that executes in current thread (main)
+
+    println("Main Program start :- ${Thread.currentThread().name}") // main thread
+
+    val job : Job = launch {// Thread : main
+        println("Fake work starts: ${Thread.currentThread().name}") // Thread: main
+        delay(1000) // Coroutine is suspended but Thread : main is free (not blocked)
+        println("Fake work finished:- ${Thread.currentThread().name}")
+        //Either main thread or some other thread
+    }
+
+    job.join()// join() to delay time
+
+    println("Main program ends :- ${Thread.currentThread().name}")
+
+}
